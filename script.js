af0b11bdce66b4aeea990a8f3aa98921cc3d669b
TKD075 <kd1419075@st.kobedenshi.ac.jp>
2025-02-07 19:45:10 +0900

    間違いからボタン押下で背景色が変更されたのが面白かったので、ボタンの色と背景色を補色にするよう変更

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById("myButton").addEventListener("click", function() {
-    let randomColor= Math.floor(Math.random()*16777215).toString(16);
-    this.style.backgroundColor = "#" + randomColor;
+    let randomColor= Math.floor(Math.random()*16777215);
+    this.style.backgroundColor = "#" + randomColor.toString(16);
+    document.body.style.backgroundColor = "#" + (16777215 - randomColor).toString(16);
 });
 
 document.getElementById("hamburger").addEventListener("click", function() {
