5a990b1fab0d5e2b08c713f51632b4b39e322059
TKD075 <kd1419075@st.kobedenshi.ac.jp>
2025-02-07 19:59:03 +0900

    ボタンをクリックしても色が変化しなかったので16進6桁でパディングするよう変更

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("myButton").addEventListener("click", function() {
     let randomColor= Math.floor(Math.random()*16777215);
-    this.style.backgroundColor = "#" + randomColor.toString(16);
-    document.body.style.backgroundColor = "#" + (16777215 - randomColor).toString(16);
+    this.style.backgroundColor = "#" + randomColor.toString(16).padStart(6, "0");
+    document.body.style.backgroundColor = "#" + (16777215 - randomColor).toString(16).padStart(6, "0");
 });
 
 document.getElementById("hamburger").addEventListener("click", function() {
