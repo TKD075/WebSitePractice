57b565c7b0e6ec8f6bb240b033238485f2791039
TKD075 <kd1419075@st.kobedenshi.ac.jp>
2025-03-06 16:57:40 +0900

    全ページ共通で使用するヘッダーをページ分け、自己紹介ページの追加、SQL連携練習用のページを追加 それに伴いフォルダを修正

---
+++ frontend/common/script.js
@@ -0,0 +1,27 @@
+// 共通のヘッダーを読み込む
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("/frontend/common/header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML("afterbegin", data);   // ページの先頭に挿入
+            setupHamburgerMenu();   // ハンバーガーアイコンのクリックイベント
+        });
+});
+
+function setupHamburgerMenu() {
+    const hamburger = document.getElementById("hamburger");
+    const menu = document.getElementById("menu");
+
+    if(hamburger&&menu){
+        hamburger.addEventListener("click", function() {
+            this.classList.toggle("active");
+            menu.classList.toggle("active");
+        });
+    }
+}
+
+document.getElementById("myButton").addEventListener("click", function() {
+    let randomColor= Math.floor(Math.random()*16777215);
+    this.style.backgroundColor = "#" + randomColor.toString(16).padStart(6, "0");
+    document.body.style.backgroundColor = "#" + (16777215 - randomColor).toString(16).padStart(6, "0");
+});
